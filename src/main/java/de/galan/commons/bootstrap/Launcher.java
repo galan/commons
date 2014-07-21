5819a85bfa523b76122ff93bf78a560bea015e25
Daniel Galán y Martins <git@galan.de>
2014-07-21 21:00:35 +0200

    launcher

---
+++ src/main/java/de/galan/commons/bootstrap/Launcher.java
@@ -0,0 +1,14 @@
+package de.galan.commons.bootstrap;
+
+/**
+ * daniel should have written a comment here.
+ *
+ * @author daniel
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Snake, Guice, dirs, Application, JMX
+	}
+
+}
