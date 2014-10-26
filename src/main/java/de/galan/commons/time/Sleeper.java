17cbec07bae0a2a32a66e7510b5e25713f2c9c04
Daniel Galán y Martins <git@galan.de>
2014-10-26 18:13:30 +0100

    reset interrupted flag

--- src/main/java/de/galan/commons/time/Sleeper.java
+++ src/main/java/de/galan/commons/time/Sleeper.java
@@ -2,14 +2,14 @@ package de.galan.commons.time;
 
 /**
  * Encapsulates the InterruptedException from Thread.sleep(ms).
- * 
+ *
  * @author daniel
  */
 public class Sleeper {
 
 	/**
 	 * Sleep a given time (time is parsed with HumanTime)
-	 * 
+	 *
 	 * @param humanTime The time in a format such as 30m10s10ms
 	 */
 	public static void sleep(String humanTime) {
@@ -19,7 +19,7 @@ public class Sleeper {
 
 	/**
 	 * Sleep the given milliseconds, swallowing the exception
-	 * 
+	 *
 	 * @param millis time in milliseconds
 	 */
 	public static void sleep(long millis) {
@@ -27,7 +27,7 @@ public class Sleeper {
 			Thread.sleep(millis);
 		}
 		catch (InterruptedException ex) {
-			// nada
+			Thread.currentThread().interrupt();
 		}
 	}
 
