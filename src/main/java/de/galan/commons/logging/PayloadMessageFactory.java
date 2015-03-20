7ce8fc990f34eb0a5d61b5737ff0f96907eacc0b
Daniel Galán y Martins <git@galan.de>
2015-03-20 20:32:56 +0100

    payload message for logging

---
+++ src/main/java/de/galan/commons/logging/PayloadMessageFactory.java
@@ -0,0 +1,22 @@
+package de.galan.commons.logging;
+
+import org.apache.logging.log4j.message.AbstractMessageFactory;
+import org.apache.logging.log4j.message.Message;
+
+
+/**
+ * Construction of PayloadMessages
+ *
+ * @author daniel
+ */
+public class PayloadMessageFactory extends AbstractMessageFactory {
+
+	public static final PayloadMessageFactory INSTANCE = new PayloadMessageFactory();
+
+
+	@Override
+	public Message newMessage(String message, Object... params) {
+		return new PayloadMessage(message, params);
+	}
+
+}
