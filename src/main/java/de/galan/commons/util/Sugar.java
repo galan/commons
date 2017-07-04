bc63d05c9df488a486910cbe75249e2502af0890
Daniel Galán y Martins <git@galan.de>
2017-07-04 14:01:13 +0200

    Updated dependencies

--- src/main/java/de/galan/commons/util/Sugar.java
+++ src/main/java/de/galan/commons/util/Sugar.java
@@ -1,6 +1,7 @@
 package de.galan.commons.util;
 
 import java.util.Optional;
+import java.util.function.Predicate;
 
 
 /**
@@ -16,6 +17,11 @@ public class Sugar {
 
 	public <T> T fallback(T val, T fallback) {
 		return val != null ? val : fallback;
+	}
+
+
+	public static <T> Predicate<T> not(Predicate<T> t) {
+		return t.negate();
 	}
 
 }
