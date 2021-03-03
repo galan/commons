03235ea7521d75169bff65c71ef189f865596033
Daniel Galán y Martins <git@galan.de>
2021-03-03 16:24:57 +0100

    Updated parent

--- src/test/java/de/galan/commons/test/jupiter/SimpleWebserverExtensionTest.java
+++ src/test/java/de/galan/commons/test/jupiter/SimpleWebserverExtensionTest.java
@@ -1,9 +1,12 @@
 package de.galan.commons.test.jupiter;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import de.galan.commons.logging.Say;
+import de.galan.commons.net.flux.HttpClientException;
 
 
 /**
@@ -14,12 +17,21 @@ public class SimpleWebserverExtensionTest {
 	@RegisterExtension
 	SimpleWebserverExtension ext = new SimpleWebserverExtension();
 
-
 	@Test
-	public void tst() {
+	public void tst() throws HttpClientException, IOException {
 		ext.startServer((req, resp) -> {
 			Say.info("WIP");
+			//	resp.setStatus(Status.OK);
+			//	try {
+			//		resp.getPrintStream().write("hello".getBytes(UTF_8));
+			//	}
+			//	catch (IOException ex) {
+			//		Say.warn("Unspecified error from daniel", ex);
+			//	}
 		});
+		//Response response = Flux.request("http://localhost:12345").get();
+		//String stream = response.getStreamAsString();
+		//Say.info("Stream: {}", stream);
 	}
 
 }
